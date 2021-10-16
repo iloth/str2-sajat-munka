f3466272e6928dafa8ed12c3dc5c8a004132f0e3
iloth <loth.istvan@hotmail.com>
2021-10-16 19:46:27 +0200

    Beutify & form submit bugfix

--- html-feladatok-bemutatkozo/js/main.js
+++ html-feladatok-bemutatkozo/js/main.js
@@ -1,36 +1,37 @@
-function menubars_click(){
+function menubars_click() {
     var nav = document.querySelector("nav");
 
-    if(!nav.style.display || nav.style.display === "none") {
+    if (!nav.style.display || nav.style.display === "none") {
         nav.style.display = "inline-block";
     } else {
         nav.style.display = "none";
     }
-    
+
 }
 
-function scrolltop_click(){
+function scrolltop_click() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-function galleryImage_click(img){
+function galleryImage_click(img) {
     var wrapper = document.querySelector("article.gallery div.wrapper");
     var fullImg = document.querySelector("article.gallery div.wrapper img");
 
-    fullImg.src=img.getAttribute("src");
-    wrapper.style.display="block";
+    fullImg.src = img.getAttribute("src");
+    wrapper.style.display = "block";
 }
 
-function fullImage_click(){
+function fullImage_click() {
     var wrapper = document.querySelector("article.gallery div.wrapper");
     var fullImg = document.querySelector("article.gallery div.wrapper img");
 
-    fullImg.src="";
-    wrapper.style.display="none";
+    fullImg.src = "";
+    wrapper.style.display = "none";
 }
 
 function message_onsubmit(e) {
-    e.preventDefault;
-    
+    e.preventDefault();
+    alert("Not implemented yet. So sorry.");
+    return false;
 }
