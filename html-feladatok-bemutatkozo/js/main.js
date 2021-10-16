c29f0ca2c792d2f7415e94f0ac9d21ecea4a6c3c
iloth <loth.istvan@hotmail.com>
2021-10-16 16:58:07 +0200

    gallery.html

--- html-feladatok-bemutatkozo/js/main.js
+++ html-feladatok-bemutatkozo/js/main.js
@@ -13,3 +13,19 @@ function scrolltop_click(){
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function galleryImage_click(img){
+    var wrapper = document.querySelector("article.gallery div.wrapper");
+    var fullImg = document.querySelector("article.gallery div.wrapper img");
+
+    fullImg.src=img.getAttribute("src");
+    wrapper.style.display="block";
+}
+
+function fullImage_click(){
+    var wrapper = document.querySelector("article.gallery div.wrapper");
+    var fullImg = document.querySelector("article.gallery div.wrapper img");
+
+    fullImg.src="";
+    wrapper.style.display="none";
+}
