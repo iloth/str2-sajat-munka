582eb027c96ba24024d29afb8bd4a14e6411ad32
iloth <loth.istvan@hotmail.com>
2021-10-28 19:29:35 +0200

    Functionality: complete, Remains: some css

--- js-1-feladatok-modal/assets/js/modal.js
+++ js-1-feladatok-modal/assets/js/modal.js
@@ -1,12 +1,46 @@
-function modalOpen(title, htmlContent) {
-    const modalTitle = document.querySelector(".modal__title");
-    const modalContent = document.querySelector(".modal__content");
-    const modal = document.querySelector(".modal__background");
+const modalOverlay = document.querySelector(".modal__overlay");
+const modalTitle = modalOverlay.querySelector(".modal__title");
+const modalContent = modalOverlay.querySelector(".modal__content");
+const modalClose = modalOverlay.querySelector(".modal__close");
+const modalCancel = modalOverlay.querySelector(".modal__cancel");
+const modalOk = modalOverlay.querySelector(".modal__ok");
 
-    modalTitle.innerHTML = title;
-    modalContent.innerHTML = htmlContent;
+let modalCallBackFunction;
 
-    modal.style.display = "block";
+function openModal({
+            title = null,
+            content = null,
+            showClose = true,
+            showCancel = true,
+            showOk = true,
+            closeOnBgClick = true
+        } = {}, onClose = null) {
+
+    if (title) {
+        modalTitle.innerHTML = title;
+    }
+    if (content) {
+        modalContent.innerHTML = content;
+    }
+
+    modalClose.style.display = showClose ? "inline-block" : "none";
+    modalCancel.style.display = showCancel ? "inline-block" : "none";
+    modalOk.style.display = showOk ? "inline-block" : "none";
+
+    modalOverlay.dataset.closeOnClick = closeOnBgClick;
+
+    modalCallBackFunction = onClose;
+
+    modalOverlay.style.display = "block";
+    modalOk.focus();
+}
+
+function closeModal(source) {
+    if (modalCallBackFunction) {
+        modalCallBackFunction(source);
+    }
+
+    modalOverlay.style.display = "none";
 }
 
 function modalMainClick(e) {
@@ -14,21 +48,19 @@ function modalMainClick(e) {
 }
 
 function modalBackgroundClick() {
-    const modal = document.querySelector(".modal__background");
-    modal.style.display = "none";
+    if(modalOverlay.dataset.closeOnClick == "true") {
+        closeModal("overlay");
+    }
 }
 
 function modalCloseClick() {
-    const modal = document.querySelector(".modal__background");
-    modal.style.display = "none";    
+    closeModal("close");
 }
 
 function modalCancelClick() {
-    const modal = document.querySelector(".modal__background");
-    modal.style.display = "none";    
+    closeModal("cancel");
 }
 
 function modalOkClick() {
-    const modal = document.querySelector(".modal__background");
-    modal.style.display = "none";    
+    closeModal("ok");
 }
