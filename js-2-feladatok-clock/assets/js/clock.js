3cdd08ce9dba30c54e2219175ce429dfadcbcd9b
iloth <loth.istvan@hotmail.com>
2021-11-06 10:39:25 +0100

    js2-clock: simplify (missing file)

--- js-2-feladatok-clock/assets/js/clock.js
+++ js-2-feladatok-clock/assets/js/clock.js
@@ -1,18 +1,15 @@
 class Clock {
-    constructor(targetElement) {
-        this._hourDiv = targetElement.querySelector(".clock__hour");
-        this._minuteDiv = targetElement.querySelector(".clock__minute");
-        this._secondDiv = targetElement.querySelector(".clock__second");
+  constructor(targetElement) {
+    this.clockLabel = targetElement.querySelector('.clock__label');
+    this.refreshScreen();
+  }
 
-        this.refreshScreen();
-    }
+  refreshScreen() {
+    const currDate = new Date();
+    this.clockLabel.textContent = currDate.toLocaleTimeString(navigator.userLanguage, { timeFormat: 'long' });
 
-    refreshScreen() {
-        const currDate = new Date();
-        this._hourDiv.innerHTML = currDate.getHours().toString().padStart(2, "0");
-        this._minuteDiv.innerHTML = currDate.getMinutes().toString().padStart(2, "0");
-        this._secondDiv.innerHTML = currDate.getSeconds().toString().padStart(2, "0");
-
-        setTimeout(() => { this.refreshScreen() }, 100);
-    }
+    setTimeout(() => {
+      this.refreshScreen();
+    }, 500);
+  }
 }
